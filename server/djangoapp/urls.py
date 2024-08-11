3f3b29be3adbe76d0f9c0ac87f83893153d8b503
lolnoor <n.fatima9797@gmail.com>
2024-08-11 13:14:56 -0400

    added views and restapis for review details

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
@@ -11,8 +12,13 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='register', view=views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='get_cars')
-    # path for dealer reviews view
+    path(route='get_cars', view=views.get_cars, name ='get_cars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='review_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+# path for dealer reviews view
 
     # path for add a review view
 

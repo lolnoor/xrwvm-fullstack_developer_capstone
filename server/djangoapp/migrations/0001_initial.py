fc94d6aa4e1eccd8b9bd1cce5a830c3cfa78dcc6
lolnoor <47978751+lolnoor@users.noreply.github.com>
2024-08-14 19:54:30 +0300

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -36,8 +36,7 @@ class Migration(migrations.Migration):
                 ('year', models.IntegerField(default=2024,
                                              validators=[django.core.validators.MaxValueValidator(2024),
                                                          django.core.validators.MinValueValidator(2015)])),
-                ('car_make', models.ForeignKey(on_delete=
-                                               django.db.models.deletion.CASCADE,
+                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
                                                to='djangoapp.carmake')),
             ],
         ),

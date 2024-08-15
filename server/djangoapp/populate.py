c006665780890300cc852f15d5b94413901a035e
lolnoor <47978751+lolnoor@users.noreply.github.com>
2024-08-16 01:04:44 +0300

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -10,11 +10,9 @@ def initiate():
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
-
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
-
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [

a042b33ffc10541e399677f28e96377aa949b83e
lolnoor <47978751+lolnoor@users.noreply.github.com>
2024-08-16 01:11:46 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -77,11 +77,11 @@ def registration(request):
         return JsonResponse(data)
 
 
-#getcar method
+# getcar method
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
